41bb7769b799f839245ace7d6bec24cd6cd77e2b
ryker123662 <adamtomasz96@gmail.com>
2023-05-09 23:44:01 +0200

    n

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,23 +6,28 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+	const router = useRouter();
 	const { data: session } = useSession();
 
 	const [posts, setPosts] = useState("");
 
 	useEffect(() => {
 		const fetchPosts = async () => {
-			const res = await fetch(`api/users${session?.user.id}/posts`);
+			const res = await fetch(`/api/users${session?.user.id}/posts`);
 			const data = await res.json();
 
 			setPosts(data);
 		};
 
 		if (session?.user.id) fetchPosts();
-	}, []);
+	}, [session?.user.id]);
 
-	const handleEdit = () => {};
-	const handleDelete = async () => {};
+	const handleEdit = (post) => {
+		router.push(`/update-prompt?id=${post._id}`);
+	};
+	const handleDelete = async () => {
+		const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+	};
 
 	return (
 		<Profile
